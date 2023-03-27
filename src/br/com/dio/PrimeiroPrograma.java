b24aaa40593cb5dfcf905d0631df8faecff7d94f
JOAO CRUZ <jsabino765@gmail.com>
2023-03-27 15:26:27 -0300

    Primeiro Commit do Teste de Intellij Idea

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    // Para inserir o Método Main, basta escrever "MAIN" + "CTRL + BARRA DE ESPAÇO"
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        //int a = 5;
+        //int b = 5;
+        //System.out.println("Hello Word! Sabino o resultado é:  " + (a + b));
+
+
+    }
+}
